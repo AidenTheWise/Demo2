b3bda24985d6d533fa117e41377c8f23027a7ab4
Aiden Wise <awise1too@gmail.com>
2023-10-02 22:56:50 -0700

    Added Cat function on Cat.java

--- Cat.java
+++ Cat.java
@@ -1,9 +1,21 @@
 package test;
 
-public class Cat {
+public class TestSuite {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Cat");
+	}
+}
+
+public class Cat {
+	
+	private String name;
+	private int age;
+	
+	public Cat(String name, int age) {
+		
+		this.name = name;
+		this.age = age;
 	}
 }
