ebbce6f8945be5701f62ce212634bc299e961f5c
Aiden Wise <awise1too@gmail.com>
2023-10-02 23:13:17 -0700

    Updated Dog,java

--- Dog.java
+++ Dog.java
@@ -1,10 +1,17 @@
 package test;
 
-public class Dog {
+
+public class TestSuite {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Dog");
 	}
 
+}
+
+public class Dog {
+
+	private String name;
+	private int age;
 }
